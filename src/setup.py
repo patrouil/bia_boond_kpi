a5f235ca6d0ec26a333a47b62e1c6063b42e273e
Patrick R. <63794182+patrouil@users.noreply.github.com>
2025-01-04 16:51:50 +0100

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Boond KPI',
+      version='0.0',
+      description='Boond Manager KPI reporting generator',
+      author='patrouil',
+      author_email='patrouil@gmail.com',
+      url='https://github.com/patrouil/bia_boond_kpi/',
+      packages=[],
+     )
